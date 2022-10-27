026cd22d10a73ad8db0d9c3a96ec27908d40465d
FMDikici <112941228+FMDikici@users.noreply.github.com>
2022-10-27 11:56:56 +0300

    Add files via upload
    Dersleri Ekledim

---
+++ main10.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) {
+	
+/*	int i;
+	
+	for(i=1;i<10;i++)
+	{
+		printf("gunaydin Balikesir\n");
+	}
+	*/
+	
+	/*int j;
+	
+	for(j=1;j<50;j++)
+	{
+		printf("%d gunaydinlar efenim\n", j);
+	}
+*/	
+	
+int k;
+
+for(k=2;k<=20;k++)
+{
+ printf("hello\n");	
+}	
+	
+	return 0;
+}

15958496b90c0fadb1a49397386138818f654f8b
FMDikici <112941228+FMDikici@users.noreply.github.com>
2022-10-27 11:57:52 +0300

    dersleri ekledim

---
+++ main11.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main(int argc, char *argv[]) {
+/*	
+int sayac=0;
+int i;
+
+for(i=1;i<=10;i++)
+{
+	sayac=sayac+i;
+}
+
+printf("sayac=%d", sayac);
+*/
+
+/*
+int i; 
+int faktor=1;
+
+for(i=1;i<=6;i++)
+{
+	faktor=faktor*i;
+}	
+printf("6 sayisinin faktoriyeli %d", faktor);
+
+*/
+
+/*
+int i,sayi;
+int faktor=1;
+
+printf("faktoriyeli alinacak sayi: ");
+scanf("%d", &sayi);
+
+for(i=1;i<=sayi;i++)
+{
+	faktor=faktor*i;
+}
+	printf("sonuc: %d",faktor);
+*/	
+	return 0;
+}

52e6ef83bbb94fddaa83f04073b5e4d697ae6035
FMDikici <112941228+FMDikici@users.noreply.github.com>
2022-12-18 01:46:12 +0300

    ders
    ekleme

---
+++ main48.c
@@ -0,0 +1,88 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*void listele(){
+printf("ali Yilmaz\n");
+printf("fatih\n");
+printf("mehmet\n");
+printf("dikici\n");
+printf("merve");
+}
+
+//void geri çaðýrmayan bir fonksiyondur	
+
+int main() {
+	
+	listele();
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	return 0;
+}
+*/
+
+//geri çaðýrmalý fonksiyon
+/*
+int topla(int x1,int x2){
+	return(x1+x2);
+}
+int main(){
+	
+	int x1,x2;
+	printf("x1 ve x2 sayisini giriniz:");
+	scanf("%d %d",&x1,&x2);
+	printf("Toplam: %d",topla(x1,x2));
+	
+	return 0;
+}
+*/
+//ders 3.1
+/*
+int carp(int x){
+	int kup;
+	kup=x*x*x;
+	return kup;
+}
+
+int main(){
+
+int s;
+printf("bir sayi giriniz:");
+scanf("%d", &s);
+printf("sonuc: %d", carp(s));
+*/	
+//ders3.2	
+/*	
+void dortgen(int x, int y)
+{
+	int i,j;
+	for(i=0;i<x;i++)
+	{
+		for(j=0;j<y;j++)
+		{
+			printf("*");
+		}
+		printf("\n");
+	}
+	}	
+	
+	int main(){
+	
+	dortgen(4,4);
+	
+	return 0;
+}	
+*/	
+
+
+	
+	
+
